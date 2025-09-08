39a9085c89c802f1b7345d30a3039a83f2a0db80
Dev-Nandgavkar <devnandgavkar@gmail.com>
2025-09-08 15:08:43 +0530

    updated prices

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,29 +1,3 @@
-const products = [{
-    image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-    name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-    rating: {
-        stars: 4.5,
-        count: 84
-    },
-    pricePaisa: 99900
-}, {
-    image: 'images/products/intermediate-composite-basketball.jpg',
-    name: 'Intermediate Size Basketball',
-    rating: {
-        stars: 4,
-        count: 127
-    },
-    pricePaisa: 199900
-}, {
-    image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-    name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-    rating: {
-        stars: 4.5,
-        count: 56
-    },
-    pricePaisa: 450000
-}];
-
 let productsHTML = '';
 
 products.forEach((product) => {
